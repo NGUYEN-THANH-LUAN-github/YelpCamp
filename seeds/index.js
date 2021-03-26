97edbcf59972b90650ca9695925bff4df0893fd7
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-03-26 19:38:43 +0900

    407. Seeding Campgrounds

---
+++ seeds/index.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+// phải back out 1 directory
+const Campground = require('../models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database connected!");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+// remove everything in database
+const seedDB = async() => {
+    await Campground.deleteMany({});
+    // const c = new Campground({ title: 'purple field' });
+    // await c.save();
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+// close database connection
+seedDB().then(() => {
+    mongoose.connection.close();
+})
