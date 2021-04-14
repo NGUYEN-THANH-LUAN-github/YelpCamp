18cd2d485808c9da1973b8f21b7582cd9b5fd436
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-04-14 11:15:40 +0900

    441. Defining ExpressError Class

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 
@@ -34,47 +35,43 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/campgrounds', async(req, res) => {
+app.get('/campgrounds', catchAsync(async(req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
-});
+}));
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 });
-app.post('/campgrounds', async(req, res, next) => {
-    try {
-        const newCampground = new Campground(req.body.campground);
-        await newCampground.save();
-        res.redirect(`/campgrounds/${newCampground._id}`);
-    } catch (e) {
-        next(e)
-    }
-})
+app.post('/campgrounds', catchAsync(async(req, res, next) => {
+    const newCampground = new Campground(req.body.campground);
+    await newCampground.save();
+    res.redirect(`/campgrounds/${newCampground._id}`);
+}))
 
-app.get('/campgrounds/:id', async(req, res) => {
+app.get('/campgrounds/:id', catchAsync(async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async(req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit', { campground });
-})
-app.put('/campgrounds/:id', async(req, res) => {
+}))
+app.put('/campgrounds/:id', catchAsync(async(req, res) => {
     const { id } = req.params;
     // the spread ... syntax: để lấy hết key-value pairs trong 1 object literal ra (trong trg hợp này object literal là req.body.campground: { title: 'Pond Forest abc', location: 'Broomfield, Colorado abc' })
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground }, { runValidators: true, new: true });
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}))
 
-app.delete('/campgrounds/:id', async(req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async(req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}));
 
 app.use((err, req, res, next) => {
     res.send('Oh Boy, something went wrong!')
